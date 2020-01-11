feafb653f23a52df7e10a7d44d8c8342d7e39f45
Cookie0318 <58831278+Cookie0318@users.noreply.github.com>
2020-01-12 00:44:30 +0900

    [Modify]SecurityController

--- src/main/java/com/znzldhkvlwk/spring/security/SecurityController.java
+++ src/main/java/com/znzldhkvlwk/spring/security/SecurityController.java
@@ -2,6 +2,9 @@ package com.znzldhkvlwk.spring.security;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +20,11 @@ public class SecurityController {
 	
 	@RequestMapping("/login_form")
 	public String login_form() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		//이미 로그인 되어있으면 home.jsp 리턴
+		if(!(auth instanceof AnonymousAuthenticationToken)) {
+			return "home";
+		}
 		return "sec/login_form";
 	}
 }
